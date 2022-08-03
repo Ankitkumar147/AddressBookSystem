e00f68554973c47bd9077bb1bef2a086eee5c6bc
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-08-04 00:43:32 +0530

    refactor UC 6

--- src/com/Bridgelabz/AddressBook/AddressBook.java
+++ src/com/Bridgelabz/AddressBook/AddressBook.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class AddressBook {
 
     private static Map<String, Person> personMap = new HashMap<>();
+    private static Map<String, Map<String, Person>> addressBookMap = new HashMap();
     private static Scanner sc2 = new Scanner(System.in);
     //Person person = new Person();
 
@@ -20,57 +21,76 @@ public class AddressBook {
             switch (choice) {
                 case 1:
                     System.out.println("Please Enter the desired contact in the Address Book.");
-                    addPersonDetails();
+                    addBook();
                     break;
                 case 2:
-                    System.out.println("Enter to details to edit.");
-                    editContact();
+                    System.out.print("\nEnter the first name of the person to edit : ");
+                    String firstName = sc2.next();
+                    System.out.print("\nEnter the city name of the person to edit : ");
+                    String cityName = sc2.next();
+                    editContact(firstName,cityName);
                     break;
                 case 3:
-                    System.out.println(personMap.toString());
+                    System.out.println("\n\t\t"+personMap.toString());
+                    System.out.println("\n\t\t" + addressBookMap.toString());
                     break;
                 case 4:
-                    deleteContact();
+                    System.out.print("\nEnter the first name of the person to edit : ");
+                    String personName = sc2.next();
+                    System.out.print("\nEnter the city name of the person to edit : ");
+                    String city = sc2.next();
+                    deleteContact(personName, city);
                     break;
                 case 5:
                     isExit=true;
+                    break;
+                default:
+                    System.out.println("Please Select Correct option.!!!");
+                    break;
             }
         } while (!isExit);
     }
-        private static void addPersonDetails () {
-            Person person;
-            person = contactFields();
-            personMap.put(person.getFirstName(), person);
+       // private static void addPersonDetails () {
+         //   Person person;
+           // person = contactFields();
+           // personMap.put(person.getFirstName(), person);
+            //addressBookMap.put(personMap.getCity());
+           // System.out.println(personMap.toString());
+       // }
+        private static void addBook(){
+        Map<String,Person> newPersonMap = new HashMap();
+        Person newPerson;
+        newPerson = contactFields();
+        if (addressBookMap.get(newPerson.getCity()) != null){
+            newPersonMap = addressBookMap.get(newPerson.getCity());
+            newPersonMap.put(newPerson.getFirstName(),newPerson);
+            addressBookMap.put(newPerson.getCity(),newPersonMap);
+            System.out.println("\n\t\t"+addressBookMap.toString());
+        }
+        }
+
+        private static void editContact (String firstName, String cityName) {
+            personMap = addressBookMap.get(cityName);
             System.out.println(personMap.toString());
-        }
-
-        private static void editContact () {
-            System.out.println("Enter the first name of Person to edit : ");
-            String firstName = sc2.next();
-            Person newPerson = personMap.get(firstName);
-            //System.out.println(newPerson.toString());
-            if (personMap.get(firstName) != null) {
-                Person person = contactFields();
-                for (int j = 0; j < personMap.size(); j++) {
-                    if (personMap.get(firstName).getFirstName().equalsIgnoreCase(newPerson.getFirstName())) {
-                        personMap.put(person.getFirstName(), person);
-                    }
-                }
-            }
-            System.out.println("\n\t\t" + personMap.toString());
-        }
-
-        private static void deleteContact() {
-            System.out.println("Enter the first name of Person to delete : ");
-            String firstName = sc2.next();
-            Person newPerson = personMap.get(firstName);
-            System.out.println(newPerson.toString());
-            if (newPerson != null) {
-                personMap.remove(firstName);
-                System.out.println("Deleted Successfully");
+            if (addressBookMap.get(cityName).get(firstName) != null) {
+                Person editedPerson = contactFields();
+                personMap.put(editedPerson.getFirstName(), editedPerson);
+                addressBookMap.put(editedPerson.getCity(), personMap);
             } else {
-                System.out.println("Record Not Exist");
+                System.out.println("Record Not Found");
             }
+            System.out.println("\n\t\t" + addressBookMap.toString());
+        }
+        private static void deleteContact(String firstName,String cityName) {
+            Person newPerson = addressBookMap.get(cityName).get(firstName);
+            System.out.println(newPerson.toString());
+           // if (addressBookMap.get(cityName).get(firstName) != null ) {
+                addressBookMap.get(cityName).remove(firstName);
+                System.out.println("Deleted Successfully");
+           // } else {
+             //   System.out.println("Record Not Exist");
+
+            System.out.println(addressBookMap.toString());
         }
         private static Person contactFields () {
             Person person = new Person();
@@ -92,4 +112,5 @@ public class AddressBook {
             System.out.println("\n-------------------\n");
             return person;
         }
+
     }
