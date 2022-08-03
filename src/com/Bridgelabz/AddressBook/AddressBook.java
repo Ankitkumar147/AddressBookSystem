ea1fd5e3b99135f7a8b041e55daf9d292cf2c233
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-08-03 22:04:18 +0530

    Added :- edit contact method.

--- src/com/Bridgelabz/AddressBook/AddressBook.java
+++ src/com/Bridgelabz/AddressBook/AddressBook.java
@@ -1,11 +1,77 @@
 package com.Bridgelabz.AddressBook;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+
 public class AddressBook {
 
-    public static void main(String[] args) {
+    private static Map<String, Person> personMap = new HashMap<>();
+    private static Scanner sc2 = new Scanner(System.in);
+    //Person person = new Person();
 
-        System.out.println("Welcome to the Address Book.");
-        System.out.println("Please Enter the desired contact in the Address Book.");
-        new Person().Details();
+    public static void main(String[] args) throws IllegalStateException {
+        boolean isExit = false;
+        do {
+            System.out.println("Welcome to the Address Book.");
+            System.out.println("Choose the desired options :--- \n 1.Add contact.\n 2.Edit contact. \n 3.Show.");
+            int choice = sc2.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Please Enter the desired contact in the Address Book.");
+                    addPersonDetails();
+                    break;
+                case 2:
+                    System.out.println("Enter to details to edit.");
+                    editContact();
+                    break;
+                case 3:
+                    System.out.println(personMap.toString());
+                    break;
+            }
+        } while (!isExit);
     }
-}
+        private static void addPersonDetails () {
+            Person person;
+            person = contactFields();
+            personMap.put(person.getFirstName(), person);
+            System.out.println(personMap.toString());
+        }
+
+        private static void editContact () {
+            System.out.println("Enter the first name of Person to edit : ");
+            String firstName = sc2.next();
+            Person newPerson = personMap.get(firstName);
+            //System.out.println(newPerson.toString());
+            if (personMap.get(firstName) != null) {
+                Person person = contactFields();
+                for (int j = 0; j < personMap.size(); j++) {
+                    if (personMap.get(firstName).getFirstName().equalsIgnoreCase(newPerson.getFirstName())) {
+                        personMap.put(person.getFirstName(), person);
+                    }
+                }
+            }
+            System.out.println("\n\t\t" + personMap.toString());
+        }
+
+        private static Person contactFields () {
+            Person person = new Person();
+            //  Scanner sc1 = new Scanner(System.in);
+            System.out.println("Please Enter the First & Last Name.");
+            person.setFirstName(sc2.next());
+            person.setLastName(sc2.next());
+            System.out.println("Enter Complete Address.");
+            System.out.println("Enter the City Name :- ");
+            person.setCity(sc2.next());
+            System.out.println("Enter the State Name :- ");
+            person.setState(sc2.next());
+            System.out.println("Enter the Zip code of the city :-");
+            person.setZipCode(sc2.nextInt());
+            System.out.println("Enter the Contact Number :- ");
+            person.setPhoneNumber(sc2.nextLong());
+            System.out.println("Enter the E-Mail Id :- ");
+            person.setEmailId(sc2.next());
+            System.out.println("\n-------------------\n");
+            return person;
+        }
+    }
--- src/com/Bridgelabz/AddressBook/Person.java
+++ src/com/Bridgelabz/AddressBook/Person.java
@@ -1,26 +1,77 @@
 package com.Bridgelabz.AddressBook;
 
-import java.util.*;
-
 public class Person {
 
-    public void Details() {
+    //public class contacts {
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private int zipCode;
+    private long phoneNumber;
+    private String email;
 
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please Enter the First & Last Name.");
-        String firstName = keyboard.next();
-        String lastName = keyboard.next();
-        System.out.println("Enter Complete Address.");
-        System.out.println("Enter the City Name :- ");
-        String city = keyboard.next();
-        System.out.println("Enter the State Name :- ");
-        String state = keyboard.next();
-        System.out.println("Enter the Zip code of the city :-");
-        String zipcode = keyboard.next();
-        System.out.println("Enter the Contact Number :- ");
-        String phoneNumber = keyboard.next();
-        System.out.println("Enter the E-Mail Id :- ");
-        String email = keyboard.next();
-        System.out.println("You have entered data:- "+firstName+" "+lastName+","+city+","+state+","+zipcode+","+phoneNumber+","+email+".");
+    //public Person(String firstName, String lastName, String city,String state,int zipCode,long phoneNumber,String email){
+    //  this.firstName = firstName;
+    //this.lastName = lastName;
+    //this.city = city;
+    //this.state = state;
+    //this.zipCode = zipCode;
+    //    this.phoneNumber = phoneNumber;
+    //  this.email = email;
+    // }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setEmailId(String email) {
+        this.email = email;
+    }
+    @Override
+    public String toString(){
+        return "[ Person ] { \n Full Name :- "+firstName+" "+lastName +"\n Address----> \n City :- "+city+"\n State :- "+state+"\n Postal Code :- "+zipCode+"\n Contact Details -----> "+"\n Phone NUmber :- "+phoneNumber+"\n Email Id :- "+email+ "\n}"+"\n<------------------------------>\n";
     }
 }
